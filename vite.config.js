93f88a138b247504246c8b9e6ca832cf7516cddc
Sammynator <sgerules@gmail.com>
2025-01-31 10:15:38 +0100

    added base

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/isitthis/", //necesarry for getting page up
+  base: "/isitsoup/", //necesarry for getting page up
   plugins: [
     vue(),
     vueDevTools(),
